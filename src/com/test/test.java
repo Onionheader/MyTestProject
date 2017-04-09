b2621ba5d53102d8291d38b806317e7372a29e07
Onionheader <martinxh@163.com>
2017-04-09 11:13:16 +0800

    第一次本地修改

--- src/com/test/test.java
+++ src/com/test/test.java
@@ -5,6 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello GitHub!");
+		System.out.println("第一次本地修改！");
 	}
 
 }
